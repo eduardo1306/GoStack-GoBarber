4b7effd4abde41e908b146e2bc46f890c9d3da6e
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-08-03 21:58:43 -0300

    Mail Provider has been created

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -3,7 +3,15 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProviders/models/IStorageProvider';
 import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
 
+import IMailProvider from './MailProviders/models/IMailProvider';
+import EtherealMailProvider from './MailProviders/implementations/EtherealMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerSingleton<IMailProvider>(
+  'MailProvider',
+  EtherealMailProvider,
 );
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -14,7 +14,7 @@ const port = 3333;
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.tmpFolder));
+app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
