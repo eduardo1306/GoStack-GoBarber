5b3bec165bedb077c16dcbd0fb192bcdd0b36990
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-08-11 22:59:26 -0300

    List Providers Service and Test has been created

---
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -0,0 +1,26 @@
+import { inject, injectable } from 'tsyringe';
+
+import IUserRepository from '@modules/users/repositories/IUserRepository';
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  user_id: string;
+}
+
+@injectable()
+class ListProvidersSerivce {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUserRepository,
+  ) {}
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.userRepository.findAllProviders({
+      except_user_id: user_id,
+    });
+
+    return users;
+  }
+}
+
+export default ListProvidersSerivce;
