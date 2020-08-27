1f433d40b9944da34ec679eae2dba6828e52e197
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-08-27 10:50:54 -0300

    Refactoring Mail and Storage providers

--- backend/src/shared/container/providers/index.ts
+++ backend/src/shared/container/providers/index.ts
@@ -1,28 +1,3 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-import IStorageProvider from './StorageProviders/models/IStorageProvider';
-import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
-
-import IMailProvider from './MailProviders/models/IMailProvider';
-import EtherealMailProvider from './MailProviders/implementations/EtherealMailProvider';
-import SparkpostMailProvider from './MailProviders/implementations/SparkpostMailProvider';
-
-import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-);
-
-container.registerInstance<IMailProvider>(
-  'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SparkpostMailProvider),
-);
+import './MailTemplateProvider';
+import './MailProviders';
+import './StorageProviders';
