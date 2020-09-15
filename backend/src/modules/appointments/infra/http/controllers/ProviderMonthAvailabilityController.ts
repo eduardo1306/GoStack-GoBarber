874f5062c9802f460d1430f444af45607b2e1fa9
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-09-14 23:34:46 -0300

    using query params on provider controller

--- backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderMonthAvailabilityController.ts
@@ -5,7 +5,7 @@ import ListProviderMonthsAvailabilityService from '@modules/appointments/service
 export default class ProviderMonthAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year } = request.body;
+    const { month, year } = request.query;
 
     const listProviderMonthAvailability = container.resolve(
       ListProviderMonthsAvailabilityService,
@@ -13,8 +13,8 @@ export default class ProviderMonthAvailabilityController {
 
     const availability = await listProviderMonthAvailability.execute({
       provider_id,
-      year,
-      month,
+      year: Number(year),
+      month: Number(month),
     });
 
     return response.json(availability);
