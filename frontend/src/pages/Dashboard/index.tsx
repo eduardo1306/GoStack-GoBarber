cedc327146ccf46b30b0c0384ece6daef20197dc
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-09-23 20:30:10 -0300

    Add style on appointment

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../../hooks/auth';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Calendar,
+  Schedule,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -28,6 +37,31 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/49220311?s=460&u=85d2f26357b4215393d9fbe437af324c67d84a81&v=4"
+                alt="Eduardo Ferreira"
+              />
+              <strong>Eduardo Ferreira</strong>
+              <span>
+                <FiClock />
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 };
