85649536f2ba861ed42b1cae687658ec4e205b38
Eduardo Ferreira <eduardoferreira@icloud.com>
2020-10-19 21:52:23 -0300

    Add appointments list

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -14,8 +14,15 @@ interface SignInCredentials {
   password: string;
 }
 
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface IAuthContext {
-  user: object;
+  user: IUser;
   loading: boolean;
   signIn: (credentials: SignInCredentials) => Promise<void>;
   signOut: () => void;
@@ -23,7 +30,7 @@ interface IAuthContext {
 
 interface AuthState {
   token: string;
-  user: object;
+  user: IUser;
 }
 
 const AuthContext = createContext<IAuthContext>({} as IAuthContext);
@@ -40,6 +47,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -61,6 +70,8 @@ const AuthProvider: React.FC = ({ children }) => {
         ['@GoBarber:token', token],
         ['@GoBarber:user', JSON.stringify(user)],
       ]);
+
+      api.defaults.headers.authorization = `Bearer ${token}`;
 
       setData({ token, user });
     },
