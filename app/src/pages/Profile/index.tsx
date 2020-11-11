0a741a09e6223084bb64a8f1b2003a75878732e7
Eduardo Ferreira <eduardoferreira@MacBook-Pro-de-Eduardo.local>
2020-11-10 21:57:31 -0300

    GoBarber Mobile has been finished

--- app/src/pages/Profile/index.tsx
+++ app/src/pages/Profile/index.tsx
@@ -7,6 +7,7 @@ import {
   TextInput,
   Alert,
 } from 'react-native';
+import ImagePicker from 'react-native-image-picker';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
@@ -119,12 +120,49 @@ const SignUp: React.FC = () => {
         );
       }
     },
-    [navigation],
+    [navigation, updateUser],
   );
 
   const handleGoBack = useCallback(() => {
     navigation.goBack();
   }, [navigation]);
+
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Selecione um avatar',
+        cancelButtonTitle: 'Cancelar',
+        takePhotoButtonTitle: 'Usar câmera',
+        chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      },
+      response => {
+        if (response.didCancel) {
+          return;
+        }
+
+        if (response.error) {
+          Alert.alert(
+            'Erro ao atualizar o avatar',
+            'Algo aconteceu de errado... Tente novamente.',
+          );
+
+          return;
+        }
+
+        const data = new FormData();
+
+        data.append('avatar', {
+          type: 'image/jpg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        });
+
+        api.patch('users/avatar', data).then(apiResponse => {
+          updateUser(apiResponse.data);
+        });
+      },
+    );
+  }, [updateUser, user.id]);
   return (
     <>
       <KeyboardAvoidingView
@@ -140,7 +178,7 @@ const SignUp: React.FC = () => {
             <BackButton onPress={handleGoBack}>
               <Icon name="chevron-left" size={24} color="#999591" />
             </BackButton>
-            <UserAvatarButton onPress={() => {}}>
+            <UserAvatarButton onPress={handleUpdateAvatar}>
               <UserAvatar source={{ uri: user.avatar_url }} />
             </UserAvatarButton>
             <View>
@@ -194,8 +232,7 @@ const SignUp: React.FC = () => {
                 textContentType="newPassword"
                 returnKeyType="next"
                 onSubmitEditing={() =>
-                  passwordConfirmationInputRef.current?.focus()
-                }
+                  passwordConfirmationInputRef.current?.focus()}
               />
               <Input
                 ref={passwordConfirmationInputRef}
